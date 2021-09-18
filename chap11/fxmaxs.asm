146c76603a96222a0f37ac4054afb6247329a718
t0mmy <tommycsth61@gmail.com>
2021-09-19 04:18:44 +0900

    chap11

---
+++ chap11/fxmaxs.asm
@@ -0,0 +1,110 @@
+  .data
+  .globl x
+x: .word 0
+  .globl y
+y: .word 0
+  .globl ymax1
+ymax1: .word 0
+  .globl ymax2
+ymax2: .word 0
+
+  .text
+  .globl main
+main:
+  # ymax1 = 0
+  la $t0, ymax1
+  sw $zero, 0($t0)
+  # ymax2 = 0
+  la $t0, ymax2
+  sw $zero, 0($t0)
+  # x = -10
+  la $t0, x
+  li $t1, -10
+  sw $t1, 0($t0)
+
+while_start:
+  # while(x <= 10)
+  la $t0, x
+  lw $s0, 0($t0)
+  li $s1, 10
+  sle $t0, $s0, $s1
+  beqz $t0, while_end
+
+
+  # y = -(x + 1)(x + 5)(x - 3)(x - 5)
+  # (x + 1)
+  la   $t0, x
+  lw   $s0, 0($t0)
+  addi $s1, $s0, 1
+  # (x + 5)
+  la   $t0, x
+  lw   $s0, 0($t0)
+  addi $s2, $s0, 5
+  # (x - 3)
+  la   $t0, x
+  lw   $s0, 0($t0)
+  li   $t0, 3
+  sub  $s3, $s0, $t0
+  # (x - 5)
+  la   $t0, x
+  lw   $s0, 0($t0)
+  li   $t0, 5
+  sub  $s4, $s0, $t0
+
+  mul  $s1, $s1, $s2
+  mul  $s1, $s1, $s3
+  mul  $s1, $s1, $s4
+  neg  $s1, $s1
+
+  la   $t0, y
+  sw   $s1, 0($t0)
+
+  # (x < 0) && (y > ymax1)
+  la   $t0, x
+  lw   $s0, 0($t0)
+  slt  $t0, $s0, $zero
+  beqz $t0, main_2
+  la   $t0, y
+  lw   $s0, 0($t0)
+  la   $t1, ymax1
+  lw   $s1, 0($t1)
+  sgt  $t0, $s0, $s1
+  beqz $t0, main_2
+
+  # ymax1 = y
+  la   $t0, y
+  lw   $s0, 0($t0)
+  la   $t0, ymax1
+  sw   $s0, 0($t0)
+
+main_2:
+  # if(x > 0 && y > ymax2)
+  la   $t0, x
+  lw   $s0, 0($t0)
+  sgt  $t0, $s0, $zero
+  beqz $t0, main_3
+  la   $t0, y
+  lw   $s0, 0($t0)
+  la   $t1, ymax2
+  lw   $s1, 0($t1)
+  sgt  $t0, $s0, $s1
+  beqz $t0, main_3
+
+  # ymax1 = y
+  la   $t0, y
+  lw   $s0, 0($t0)
+  la   $t0, ymax2
+  sw   $s0, 0($t0)
+
+main_3:
+  # x += 1
+  la $t0, x
+  lw $s0, 0($t0)
+  addi $s0, $s0, 1
+  sw $s0, 0($t0)
+  
+  b while_start
+
+while_end:
+  li $v0, 0
+  jr $ra
